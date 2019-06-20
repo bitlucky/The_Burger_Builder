f30c01c2401d164e467d9b6fad6ac7b022775a2d
bitlucky <lokeshtiwari877@gmail.com>
2019-06-20 23:04:43 +0530

    person component updated

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const Person = ( props ) => {
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+            
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
