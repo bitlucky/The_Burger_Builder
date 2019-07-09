72a0085feae27025aac9d6e89a2e62cd83db6cf7
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 11:00:16 +0530

    Burger Builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,15 +4,25 @@ import Burger from "../../components/Burger/Burger"
 
 
 class BurgerBuilder extends Component {
+    // constructor(props){
+    //     super(props);
+    //     this.state= {...}
+    // }
+    state = {
+        ingredients : {
+            salad : 1, 
+            bacon : 1,
+            cheese : 2,
+            meat : 2
+        }
+    }
     render(){
         return (
             <Aux>
-                <Burger/>
-                <div>Build Controls</div> 
+                <Burger ingredients={this.state.ingredients}/>
             </Aux>
-            
-        );
+        )
     }
 }
 
-export default BurgerBuilder;
+export default BurgerBuilder; 
