b29a5cad80638eb1a560b2d78b9eb0bb034661c8
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 19:38:38 +0530

    add control added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,8 @@ const BuildControls = (props) => {
     return (
         <div className= {classes.BuildControls}>
             {controls.map(ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label}/>
+                <BuildControl key={ctrl.label} label={ctrl.label}
+                added = {() => props.ingredientAdded(ctrl.type)} />
             ))}
           
         </div>
