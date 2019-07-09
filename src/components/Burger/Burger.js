67b09c9119e09aa11ffd9f017db0b6d089921a43
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 11:17:14 +0530

    updated

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,22 +3,27 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)  
-        .map( igKey => {
-             return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-    });
-return (
-    <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        {transformedIngredients}
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="bread-bottom" />
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if (transformedIngredients === 0) {
+        transformedIngredients = <p>Please start adding some Ingredients</p>
+     }
+    console.log(transformedIngredients)
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
 
-    </div>
-);
+        </div>
+    );
 }
 
 export default burger;
