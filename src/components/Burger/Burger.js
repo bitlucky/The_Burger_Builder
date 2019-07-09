4c4e933b594b60a73d51ed25b766d1b93b14c4b6
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 10:59:53 +0530

    Updated

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,24 @@
 import React from "react";
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-const burger =(props) => {
-    return (
-        <div className = {classes.Burger}>
-            <BurgerIngredient type = "bread-top"/>
-            <BurgerIngredient type = "cheese"/>
-            <BurgerIngredient type = "meat"/>
-            <BurgerIngredient type = "bread-bottom"/>
 
-        </div>
-    );
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)  
+        .map( igKey => {
+             return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    });
+return (
+    <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+        {transformedIngredients}
+        <BurgerIngredient type="cheese" />
+        <BurgerIngredient type="meat" />
+        <BurgerIngredient type="bread-bottom" />
+
+    </div>
+);
 }
 
 export default burger;
