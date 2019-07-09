36cb8b25800f1cdea7ccab787e68449a9bec4c0c
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 19:38:15 +0530

    Add Control Added

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ const BuildControl = (props)=>(
     <div className = {classes.BuildControl}>
         <div className = {classes.label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
-        <button className= {classes.More}> More </button>
+        <button className= {classes.More} onClick = {props.added}> More </button>
     </div>
 )
 
