1842dae1d6796df2af93b2929430707f1b1020d2
bitlucky <lokeshtiwari877@gmail.com>
2019-07-09 18:04:09 +0530

    updated

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label : 'Salad' , type : 'salad'},
+    {label : 'Bacon' , type : 'bacon'},
+    {label : 'Cheese' , type : 'cheese'},
+    {label : 'Meat' , type : 'meat'}
+]
 
 const BuildControls = (props) => {
     return (
-        <div>
+        <div className= {classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label}/>
+            ))}
           
         </div>
     )
